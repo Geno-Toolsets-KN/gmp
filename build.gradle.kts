1268265d6e3a4de036d2c257e0d3ed3762d662bd
Y. Z. Chen <754097987@qq.com>
2022-11-22 23:20:02 +0800

    [Publishing] now it is able to publish to OSS Sonatype.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,12 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithHostTests
 
 plugins {
     kotlin("multiplatform") version "1.7.21"
+    `java-library`
+    `maven-publish`
 }
+
+group = "com.geno1024.cinterops"
+version = "0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -16,6 +21,27 @@ kotlin {
         it.compilations.getByName("main") {
             val gmp by cinterops.creating {
                 packageName = "cinterops.gmp"
+            }
+        }
+    }
+}
+
+java {
+    withSourcesJar()
+}
+
+publishing {
+    repositories {
+        mavenLocal {
+            name = "GenoLocal"
+            url = uri("/srv/http/geno1024/m2repo")
+        }
+        maven {
+            name = "OSSRH"
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            credentials {
+                username = System.getenv("ossrh_username")
+                password = System.getenv("ossrh_password")
             }
         }
     }
